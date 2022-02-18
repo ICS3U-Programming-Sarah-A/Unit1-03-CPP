575b1c19f8fbfeac2e9cc5612a8f808526a04136
Ubuntu <ubuntu@ip-172-31-55-166.ec2.internal>
2022-02-18 23:47:50 +0000

    Made helloWorldStyle.

---
+++ helloWorldStyle.cpp
@@ -0,0 +1,12 @@
+// Copyright (c) 2021 Ms Raffin All rights reserved.
+// .
+// Created by: Sarah
+// Date: Feb.18/22
+// This program displays "Hello, World with style".
+
+#include <iostream>
+
+int main() {
+    std::cout << "Hello, World with style!" << std::endl;
+}
+
